b71b3e04c38a57f3cd7d1341793af74c154ee0d6
isooo <midnightmarching@gmail.com>
2019-03-19 02:12:20 +0900

    modify application(ExchangeService) : add validation method about remittance, add instance variable for ExchangeApi

--- wirebarley/exchangeRate/src/main/java/com/example/demo/exchangeRate/application/ExchangeService.java
+++ wirebarley/exchangeRate/src/main/java/com/example/demo/exchangeRate/application/ExchangeService.java
@@ -3,31 +3,53 @@ package com.example.demo.exchangeRate.application;
 import com.example.demo.exchangeRate.domain.Country;
 import com.example.demo.exchangeRate.domain.ExchangeRate;
 import com.example.demo.exchangeRate.domain.Money;
-import com.example.demo.exchangeRate.infra.ExchangeApiClient;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
 
 @Service
 public class ExchangeService {
+    private ExchangeApi exchangeApi;
+
+    public ExchangeService(ExchangeApi exchangeApi) {
+        this.exchangeApi = exchangeApi;
+    }
 
     public ExchangeRate getRateInfo(
-            final String baseCountry,
-            final String quotesCountry
-    ) throws IOException {
-        ExchangeApiClient client = new ExchangeApiClient();
-        Country base = Country.valueOf(baseCountry);
-        Country quotes = Country.valueOf(quotesCountry);
-        return client.getExchangeRate(base, quotes);
+            final String base,
+            final String quotes
+    ) {
+        return getRateInfo(Country.valueOf(base), Country.valueOf(quotes));
     }
 
     public ExchangeResponse getAmount(
-            final String baseCountry,
-            final String quotesCountry,
+            final String base,
+            final String quotes,
             final double remittance
-    ) throws IOException {
+    ) {
+        final Country baseCountry = Country.valueOf(base);
+        final Country quotesCountry = Country.valueOf(quotes);
+        validate(remittance, baseCountry);
         final ExchangeRate exchangeRate = getRateInfo(baseCountry, quotesCountry);
         final Money exchange = exchangeRate.exchange(new Money(remittance));
         return new ExchangeResponse(exchangeRate, exchange);
+    }
+
+    private ExchangeRate getRateInfo(
+            final Country baseCountry,
+            final Country quotesCountry
+    ) {
+        return exchangeApi.getExchangeRate(baseCountry, quotesCountry)
+                .orElseGet(null)
+                ;
+    }
+
+    private void validate(final double remittance, final Country baseCountry) {
+        if (remittance < 0) {
+            throw new IllegalArgumentException("송금액이 바르지 않습니다.");
+        }
+        if (baseCountry == Country.USA) {
+            if (remittance > 10000) {
+                throw new IllegalArgumentException("송금액이 바르지 않습니다.");
+            }
+        }
     }
 }
