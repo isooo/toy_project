a0956214a20030e7c797c6b6ce04d1a740bee03f
isooo <midnightmarching@gmail.com>
2019-03-19 02:22:22 +0900

    modify application test(ExchangeServiceTest) : use Mockito

--- wirebarley/exchangeRate/src/test/java/com/example/demo/exchangeRate/application/ExchangeServiceTest.java
+++ wirebarley/exchangeRate/src/test/java/com/example/demo/exchangeRate/application/ExchangeServiceTest.java
@@ -1,22 +1,36 @@
 package com.example.demo.exchangeRate.application;
 
-import com.example.demo.exchangeRate.domain.Country;
 import com.example.demo.exchangeRate.domain.ExchangeRate;
-import com.example.demo.exchangeRate.domain.Money;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ExchangeServiceTest {
+    @Mock
+    private ExchangeApi exchangeApi;
+
+    @InjectMocks
+    private ExchangeService exchangeService;
+
     @Test
-    public void 환율_정보_구하기() throws IOException {
+    public void 환율_정보_구하기() {
         // given
-        ExchangeService service = new ExchangeService();
+        given(exchangeApi.getExchangeRate(any(), any()))
+                .willReturn(Optional.of(new ExchangeRate(any(), any(), 1000)))
+                ;
 
         // when
-        ExchangeRate exchangeRate = service.getRateInfo("USA", "PHL");
+        final ExchangeRate exchangeRate = exchangeService.getRateInfo("USA", "PHL");
 
         // then
         assertThat(exchangeRate.getRate()).isInstanceOf(Double.class);
@@ -26,13 +40,31 @@ public class ExchangeServiceTest {
     @Test
     public void 수취_금액_구하기() throws IOException {
         // given
-        ExchangeService service = new ExchangeService();
+        final double rate = 100;
+        final double remittance = 1000;
+        given(exchangeApi.getExchangeRate(any(), any()))
+                .willReturn(Optional.of(new ExchangeRate(any(), any(), rate)))
+        ;
 
         // when
-        ExchangeResponse response = service.getAmount("USA", "JPN", 1000);
-        Money amount = response.getAmount();
+        final ExchangeResponse response = exchangeService.getAmount("USA", "JPN", remittance);
 
         // then
-        assertThat(amount.getValue()).isGreaterThan(0);
+        assertThat(response.getAmount().getValue()).isEqualTo(remittance * rate);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void 수취_금액_구하기_실패() throws IOException {
+        // given
+        final double rate = 100;
+        final double remittance = -1;
+        given(exchangeApi.getExchangeRate(any(), any()))
+                .willReturn(Optional.of(new ExchangeRate(any(), any(), rate)))
+        ;
+
+        // when
+        final ExchangeResponse response = exchangeService.getAmount("USA", "JPN", remittance);
+
+        // then
     }
 }
