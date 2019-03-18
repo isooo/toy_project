ca6046db43d437572a63672d0359ff61d126ca8e
isooo <midnightmarching@gmail.com>
2019-03-19 02:21:34 +0900

    modify infra test(ExchangeApiClientTest)

--- wirebarley/exchangeRate/src/test/java/com/example/demo/exchangeRate/infra/ExchangeApiClientTest.java
+++ wirebarley/exchangeRate/src/test/java/com/example/demo/exchangeRate/infra/ExchangeApiClientTest.java
@@ -3,21 +3,28 @@ package com.example.demo.exchangeRate.infra;
 import com.example.demo.exchangeRate.domain.Country;
 import com.example.demo.exchangeRate.domain.ExchangeRate;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import java.io.IOException;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ExchangeApiClientTest {
+    @InjectMocks
+    private ExchangeApiClient client;
+
     @Test
-    public void 미국에서_한국_환율() throws IOException {
+    public void 미국에서_한국_환율() {
         // given
-        final ExchangeApiClient client = new ExchangeApiClient();
 
         // when
-        final ExchangeRate exchangeRate = client.getExchangeRate(Country.USA, Country.KOR);
+        final Optional<ExchangeRate> exchangeRate = client.getExchangeRate(Country.USA, Country.KOR);
 
         // then
-        assertThat(exchangeRate.getRate()).isGreaterThan(0);
+//        assertThat(exchangeRate.isPresent()).isTrue();
+        assertThat(exchangeRate.get().getRate()).isGreaterThan(0);
     }
 }
