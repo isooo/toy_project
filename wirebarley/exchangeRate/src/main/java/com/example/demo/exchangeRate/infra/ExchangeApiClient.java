119345ae7384d0fca6b68b881330ffa4462c9c0c
isooo <midnightmarching@gmail.com>
2019-03-19 02:09:01 +0900

    modify infra(ExchangeApiClient) : modify try/catch, modify the return type to Optional

--- wirebarley/exchangeRate/src/main/java/com/example/demo/exchangeRate/infra/ExchangeApiClient.java
+++ wirebarley/exchangeRate/src/main/java/com/example/demo/exchangeRate/infra/ExchangeApiClient.java
@@ -1,35 +1,43 @@
 package com.example.demo.exchangeRate.infra;
 
+import com.example.demo.exchangeRate.application.ExchangeApi;
 import com.example.demo.exchangeRate.domain.Country;
 import com.example.demo.exchangeRate.domain.ExchangeRate;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.IOException;
 import java.net.URI;
+import java.util.Optional;
 
 /**
  * e.g. http://apilayer.net/api/live?access_key=ee50cd7cc73c9b7a7bb3d9617cfb6b9c
  */
-public class ExchangeApiClient {
+@Component
+public class ExchangeApiClient implements ExchangeApi {
     private static final String BASE_URL = "http://apilayer.net/api/";
     private static final String ENDPOINT = "live";
     private static final String QUERY_PARAM_NAME_ACCESS_KEY = "access_key";
-    private static final String ACCESS_KEY = "ee50cd7cc73c9b7a7bb3d9617cfb6b9c";
+    private static final String ACCESS_KEY = "eb383804a4a87fe1dda15d097594a941";
     private static final ObjectMapper MAPPER = new ObjectMapper();
 
-    public ExchangeRate getExchangeRate(
+    @Override
+    public Optional<ExchangeRate> getExchangeRate(
             final Country base,
             final Country quotes
-    ) throws IOException {
-        final JsonNode root = MAPPER.readTree(getBody(getLiveUri()));
-        final double rate = root.path("quotes")
-                .path(base.getCurrencyUnit() + quotes.getCurrencyUnit())
-                .doubleValue()
-                ;
-        return new ExchangeRate(base, quotes, rate);
+    ) {
+        try {
+            final JsonNode root = MAPPER.readTree(getBody(getLiveUri()));
+            final double rate = root.path("quotes")
+                    .path(base.getCurrencyUnit() + quotes.getCurrencyUnit())
+                    .doubleValue()
+                    ;
+            return Optional.of(new ExchangeRate(base, quotes, rate));
+        } catch (Exception e) {
+            return Optional.empty();
+        }
     }
 
     private URI getLiveUri() {
