86b6947afe4513c6cd9a601f66ca05553fb64750
isooo <midnightmarching@gmail.com>
2019-03-18 15:44:53 +0900

    add property and method in Country domain class : name, getValue(), getName() and get List<Country> for select box

--- wirebarley/exchangeRate/src/main/java/com/example/demo/exchangeRate/domain/Country.java
+++ wirebarley/exchangeRate/src/main/java/com/example/demo/exchangeRate/domain/Country.java
@@ -1,18 +1,39 @@
 package com.example.demo.exchangeRate.domain;
 
-public enum Country {
-    USA("USD"),
-    KOR("KRW"),
-    JPN("JPY"),
-    PHL("PHP");
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public enum Country {
+    USA("미국", "USD"),
+    KOR("한국", "KRW"),
+    JPN("일본", "JPY"),
+    PHL("필리핀", "PHP");
+
+    private final String name;
     private final String currencyUnit;
 
-    Country(final String currencyUnit) {
+    Country(final String name, final String currencyUnit) {
+        this.name = name;
         this.currencyUnit = currencyUnit;
+    }
+
+    public String getValue() {
+        return name();
+    }
+
+    public String getName() {
+        return this.name;
     }
 
     public String getCurrencyUnit() {
         return this.currencyUnit;
+    }
+
+    public static List<Country> valuesExcludeBase(final Country base) {
+        return Arrays.stream(Country.values())
+                .filter(country -> country != base)
+                .collect(Collectors.toList())
+                ;
     }
 }
