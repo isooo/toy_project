8af0310590d9bad79cdb708457ed442008bce668
isooo <midnightmarching@gmail.com>
2019-03-19 09:53:10 +0900

    modify ui (ExchangeController) : use logger, add mapping value

--- wirebarley/exchangeRate/src/main/java/com/example/demo/exchangeRate/ui/ExchangeController.java
+++ wirebarley/exchangeRate/src/main/java/com/example/demo/exchangeRate/ui/ExchangeController.java
@@ -1,6 +1,8 @@
 package com.example.demo.exchangeRate.ui;
 
 import com.example.demo.exchangeRate.domain.Country;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,10 +10,13 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ExchangeController {
-    @GetMapping("/")
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExchangeController.class);
+
+    @GetMapping(value = {"/", "/index"})
     public ModelAndView index(
             @RequestParam(value = "base", defaultValue = "USA") String base
     ) {
+        LOGGER.info("/index {" + "base=" + base + "}");
         final Country baseCountry = Country.valueOf(base);
         final ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("base", baseCountry);
