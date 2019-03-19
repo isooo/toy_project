7ab8a2ed58679c403712fb9ca11c2660db60a9b9
isooo <midnightmarching@gmail.com>
2019-03-19 10:04:47 +0900

    modify ui(ExceptionAdvice) : for handling IllegalArgumentException and InvalidTrasferAmount

--- wirebarley/exchangeRate/src/main/java/com/example/demo/exchangeRate/ui/ExceptionAdvice.java
+++ wirebarley/exchangeRate/src/main/java/com/example/demo/exchangeRate/ui/ExceptionAdvice.java
@@ -1,5 +1,6 @@
 package com.example.demo.exchangeRate.ui;
 
+import com.example.demo.exchangeRate.domain.InvalidTransferAmount;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,8 +8,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionAdvice {
+    private static final String INVALID_REQUEST = "유효하지 않은 요청입니다.";
+
     @ExceptionHandler(value = IllegalArgumentException.class)
     public ResponseEntity<String> illegalArgumentException(final IllegalArgumentException exception) {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .body(INVALID_REQUEST)
+                ;
+    }
+
+    @ExceptionHandler(value = InvalidTransferAmount.class)
+    public ResponseEntity<String> invalidTransferAmount(final InvalidTransferAmount exception) {
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
                 .body(exception.getMessage())
                 ;
