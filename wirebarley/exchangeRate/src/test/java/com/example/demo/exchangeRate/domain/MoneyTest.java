5ae3c82728c7a6300bf837ec8a6132a543fbb479
isooo <midnightmarching@gmail.com>
2019-03-19 10:10:37 +0900

    modify domain test(Money) : modify expected exception

--- wirebarley/exchangeRate/src/test/java/com/example/demo/exchangeRate/domain/MoneyTest.java
+++ wirebarley/exchangeRate/src/test/java/com/example/demo/exchangeRate/domain/MoneyTest.java
@@ -3,7 +3,7 @@ package com.example.demo.exchangeRate.domain;
 import org.junit.Test;
 
 public class MoneyTest {
-    @Test(expected = IllegalArgumentException.class)
+    @Test(expected = InvalidTransferAmount.class)
     public void 잘못된_금액_입력() {
         // given
 
