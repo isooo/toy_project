161741815b80a827c4abd0483711e9385a370142
isooo <midnightmarching@gmail.com>
2019-03-21 22:28:30 +0900

    modify application(ExchangeService) : add method about response that changes in the base country

--- wirebarley/exchangeRate/src/main/java/com/example/demo/exchangeRate/application/ExchangeService.java
+++ wirebarley/exchangeRate/src/main/java/com/example/demo/exchangeRate/application/ExchangeService.java
@@ -6,6 +6,9 @@ import com.example.demo.exchangeRate.domain.InvalidTransferAmount;
 import com.example.demo.exchangeRate.domain.Money;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
 @Service
 public class ExchangeService {
     private static final int DOLLAR_UPPER_LIMIT = 10_000;
@@ -40,9 +43,40 @@ public class ExchangeService {
             final Country baseCountry,
             final Country quotesCountry
     ) {
+        if (baseCountry.getValue() != "USA") {
+            return twoWayQuotation(baseCountry, quotesCountry);
+        }
         return exchangeApi.getExchangeRate(baseCountry, quotesCountry)
                 .orElseGet(null)
                 ;
+    }
+
+    private ExchangeRate twoWayQuotation(
+            final Country baseCountry,
+            final Country quotesCountry
+    ) {
+        double resultRate;
+        double baseRate;
+        double quotesRate;
+        if (quotesCountry.getValue() == "USA") {
+            baseRate = getRate(quotesCountry, baseCountry);
+            quotesRate = 1;
+        } else {
+            baseRate = getRate(Country.USA, baseCountry);
+            quotesRate = getRate(Country.USA, quotesCountry);
+        }
+        resultRate = new BigDecimal(quotesRate)
+                .divide(new BigDecimal(baseRate), 8, BigDecimal.ROUND_HALF_UP)
+                .doubleValue()
+        ;
+        return Optional.of(new ExchangeRate(baseCountry, quotesCountry, resultRate)).orElseGet(null);
+    }
+
+    private double getRate(Country a, Country b) {
+        return exchangeApi
+                .getExchangeRate(a, b)
+                .get()
+                .getRate();
     }
 
     private void validate(final double remittance, final Country baseCountry) {
