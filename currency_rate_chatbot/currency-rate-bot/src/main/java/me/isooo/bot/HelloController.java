93c5ad45d1c2fb42da34928a580c800d2adf086a
isooo <midnightmarching@gmail.com>
2019-08-20 22:32:43 +0900

    #4 유저 요청을 받으면 hello를 리턴하기 - 핸들러 컨트롤러에서 이벤트 받아서 처리

--- currency_rate_chatbot/currency-rate-bot/src/main/java/me/isooo/bot/HelloController.java
+++ currency_rate_chatbot/currency-rate-bot/src/main/java/me/isooo/bot/HelloController.java
@@ -1,22 +1,54 @@
 package me.isooo.bot;
 
+import com.linecorp.bot.client.*;
+import com.linecorp.bot.model.*;
 import com.linecorp.bot.model.event.*;
 import com.linecorp.bot.model.event.message.*;
+import com.linecorp.bot.model.message.*;
+import com.linecorp.bot.model.response.*;
 import com.linecorp.bot.spring.boot.annotation.*;
+import lombok.*;
 import lombok.extern.slf4j.*;
+
+import java.util.*;
+import java.util.concurrent.*;
 
 @Slf4j
 @LineMessageHandler
 public class HelloController {
+    private LineMessagingClient lineMessagingClient;
+
+    public HelloController(LineMessagingClient lineMessagingClient) {
+        this.lineMessagingClient = lineMessagingClient;
+    }
+
     @EventMapping
     public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         log.info("event: {}", event);
-        final String originalMessageText = event.getMessage().getText();
-        log.info("originalMessageText: {}", originalMessageText);
+        final String replyToken = event.getReplyToken();
+        reply(replyToken, Collections.singletonList(new TextMessage("hello")));
+    }
+
+    @EventMapping
+    public void handleStickerMessageEvent(MessageEvent<StickerMessageContent> event) {
+        log.info("event: {}", event);
+        final String replyToken = event.getReplyToken();
+        reply(replyToken, Collections.singletonList(new TextMessage("hello")));
     }
 
     @EventMapping
     public void handleDefaultMessageEvent(Event event) {
-        log.info("event: {}", event);
+        log.info("event(no reply): {}", event);
+    }
+
+    private void reply(@NonNull String replyToken, @NonNull List<Message> messages) {
+        try {
+            BotApiResponse apiResponse = lineMessagingClient
+                    .replyMessage(new ReplyMessage(replyToken, messages))
+                    .get();
+            log.info("Sent messages: {}", apiResponse);
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
